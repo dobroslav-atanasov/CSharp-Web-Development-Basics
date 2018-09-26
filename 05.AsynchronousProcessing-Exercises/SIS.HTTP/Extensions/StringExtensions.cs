70d295d53e2ea26b859f34d1e594938862203c23
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-09-26 22:27:21 +0300

    Added extensions methods.

---
+++ 05.AsynchronousProcessing-Exercises/SIS.HTTP/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace SIS.HTTP.Extensions
+{
+    public class StringExtensions
+    {
+        public string Capitalize(string message)
+        {
+            var firstLetter = message[0].ToString().ToUpper();
+            var allOther = message.Substring(1).ToLower();
+
+            return $"{firstLetter}{allOther}";
+        }
+    }
+}
