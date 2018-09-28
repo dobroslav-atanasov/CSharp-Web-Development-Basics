ec3c873cfd6cc7dd0a5a07b1fb16f341ef67f198
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-09-28 22:58:20 +0300

    Implemented HomeController.cs and StartUp.cs

--- 05.AsynchronousProcessing-Exercises/SIS.Demo/HomeController.cs
+++ 05.AsynchronousProcessing-Exercises/SIS.Demo/HomeController.cs
@@ -1,6 +1,16 @@
 ﻿namespace SIS.Demo
 {
+    using HTTP.Enums;
+    using HTTP.Responses.Contracts;
+    using WebServer.Results;
+
     public class HomeController
     {
+        public IHttpResponse Index()
+        {
+            var content = "<h1>Hello, World!<h1>";
+
+            return new HtmlResult(content, HttpResponseStatusCode.Ok);
+        }
     }
 }
