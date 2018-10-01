f5895a1aae82d8b29f870dc09d8bce928f94fec5
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-01 19:27:51 +0300

    Added HttpCookieCollection.cs and interface IHttpCookieCollectio.cs

---
+++ 06.StateManagement-Exercises/SIS.HTTP/Cookies/HttpCookieCollection.cs
@@ -0,0 +1,41 @@
+﻿namespace SIS.HTTP.Cookies
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Contracts;
+
+    public class HttpCookieCollection : IHttpCookieCollection
+    {
+        private readonly Dictionary<string, HttpCookie> cookies;
+
+        public HttpCookieCollection()
+        {
+            this.cookies = new Dictionary<string, HttpCookie>();
+        }
+
+        public void Add(HttpCookie cookie)
+        {
+            this.cookies[cookie.Key] = cookie;
+        }
+
+        public bool ContainsCookie(string key)
+        {
+            return this.cookies.ContainsKey(key);
+        }
+
+        public HttpCookie GetCookie(string key)
+        {
+            return this.cookies[key];
+        }
+
+        public bool HasCookies()
+        {
+            return this.cookies.Any();
+        }
+
+        public override string ToString()
+        {
+            return string.Join("; ", this.cookies.Values);
+        }
+    }
+}
