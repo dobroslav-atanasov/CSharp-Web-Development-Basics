4290eb86c3038776336f017e46e9c129a7af3a10
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-01 22:07:16 +0300

    Added HttpSession.cs and interface IHttpSession.

---
+++ 06.StateManagement-Exercises/SIS.HTTP/Sessions/HttpSession.cs
@@ -0,0 +1,38 @@
+﻿namespace SIS.HTTP.Sessions
+{
+    using System.Collections.Generic;
+    using Contracts;
+
+    public class HttpSession : IHttpSession
+    {
+        private readonly Dictionary<string, object> parameters;
+
+        public HttpSession(string id)
+        {
+            this.Id = id;
+            this.parameters = new Dictionary<string, object>();
+        }
+
+        public string Id { get; }
+
+        public object GetParameter(string name)
+        {
+            return this.parameters[name];
+        }
+
+        public bool ContainsParameter(string name)
+        {
+            return this.parameters.ContainsKey(name);
+        }
+
+        public void AddParameter(string name, object parameter)
+        {
+            this.parameters[name] = parameter;
+        }
+
+        public void ClearParameters()
+        {
+            this.parameters.Clear();
+        }
+    }
+}
