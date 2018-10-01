dcab4395e11420795eea9b63a8607b1ec16aaded
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-01 19:17:17 +0300

    Created HttpCookie.cs

---
+++ 06.StateManagement-Exercises/SIS.HTTP/Cookies/HttpCookie.cs
@@ -0,0 +1,36 @@
+﻿namespace SIS.HTTP.Cookies
+{
+    using System;
+
+    public class HttpCookie
+    {
+        private const int HttpCookieDefaultExpiresDays = 3;
+
+        public HttpCookie(string key, string value, int expires = HttpCookieDefaultExpiresDays)
+        {
+            this.Key = key;
+            this.Value = value;
+            this.IsNew = true;
+            this.Expires = DateTime.UtcNow.AddDays(expires);
+        }
+
+        public HttpCookie(string key, string value, bool isNew, int expires = HttpCookieDefaultExpiresDays)
+            :this(key, value, expires)
+        {
+            this.IsNew = isNew;
+        }
+
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+
+        public DateTime Expires { get; set; }
+
+        public bool IsNew { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Key}={this.Value}; Expires={this.Expires.ToString("R")}";
+        }
+    }
+}
