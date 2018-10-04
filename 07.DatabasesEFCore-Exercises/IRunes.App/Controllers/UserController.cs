4705cdd6bcda5fb5dc812f836f3fa248d9c661e6
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-04 22:40:13 +0300

    Added login.html

---
+++ 07.DatabasesEFCore-Exercises/IRunes.App/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿namespace IRunes.App.Controllers
+{
+    using System.IO;
+    using SIS.HTTP.Enums;
+    using SIS.HTTP.Requests.Contracts;
+    using SIS.HTTP.Responses.Contracts;
+    using SIS.WebServer.Results;
+
+    public class UserController
+    {
+        public IHttpResponse Login()
+        {
+            var content = File.ReadAllText($"{Directory.GetCurrentDirectory()}/Views/login.html");
+
+            return new HtmlResult(content, HttpResponseStatusCode.Ok);
+        }
+
+        public IHttpResponse Login(IHttpRequest request)
+        {
+            var username = request.FormData["username"].ToString();
+            var password = request.FormData["password"].ToString();
+
+            return new RedirectResult("/");
+        }
+    }
+}
--- 07.DatabasesEFCore-Exercises/IRunes.App/StartUp.cs
+++ 07.DatabasesEFCore-Exercises/IRunes.App/StartUp.cs
@@ -12,6 +12,8 @@
             var serverRoutingTable = new ServerRoutingTable();
 
             serverRoutingTable.Routes[HttpRequestMethod.Get]["/"] = request => new HomeController().Index();
+            serverRoutingTable.Routes[HttpRequestMethod.Get]["/login"] = request => new UserController().Login();
+            serverRoutingTable.Routes[HttpRequestMethod.Post]["/login"] = request => new UserController().Login(request);
 
             var server = new Server(8080, serverRoutingTable);
             server.Run();
