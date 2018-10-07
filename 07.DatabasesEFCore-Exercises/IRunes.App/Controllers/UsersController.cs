9b96d34df510cbfbefba12e862374ab674e97503
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-07 13:45:54 +0300

    Finished register method.

--- 07.DatabasesEFCore-Exercises/IRunes.App/Controllers/UsersController.cs
+++ 07.DatabasesEFCore-Exercises/IRunes.App/Controllers/UsersController.cs
@@ -10,6 +10,7 @@
     {
         private const string InvalidUsernameOrPassword = "<h2>Invalid username, email or password!</h2>";
         private const string InvalidUsernameOrPasswordLength = "<h2>Invalid user data!</h2>";
+        private const string UsernameAlreadyExists = "<h2>Username already exists!</h2>";
 
         private readonly IUserService userService;
         private readonly IHashService hashService;
@@ -59,10 +60,16 @@
             var confirmPassword = request.FormData["confirmPassword"].ToString();
             var email = request.FormData["email"].ToString();
 
-            // TODO Validation
-            if (username.Length < 4 || password.Length < 4)
+            if (username.Length < 4 || password.Length < 4 || password != confirmPassword)
             {
-                this.ApplyError("");
+                this.ApplyError(InvalidUsernameOrPasswordLength);
+                return this.View();
+            }
+
+            if (this.userService.ContainsUser(username))
+            {
+                this.ApplyError(UsernameAlreadyExists);
+                return this.View();
             }
 
             var hashPassword = this.hashService.Hash(password);
@@ -71,37 +78,6 @@
             var response = new RedirectResult("/");
             this.SignInUser(username, request, response);
             return response;
-
-
-            //var username = request.FormData["username"].ToString();
-            //var password = request.FormData["password"].ToString();
-            //var confirmPassword = request.FormData["confirmPassword"].ToString();
-            //var email = request.FormData["email"].ToString();
-
-            //if (username.Length < 4 || password.Length < 4)
-            //{
-            //    //return this.BadRequestError("Invalid user data!");
-            //}
-
-            //if (password != confirmPassword)
-            //{
-            //    //return this.BadRequestError("Passwords are different!");
-            //}
-
-            //if (this.userService.ContainsUser(username))
-            //{
-            //    //return this.BadRequestError($"User {username} already exists!");
-            //}
-
-            //var hashedPassword = this.hashService.Hash(password);
-
-            //this.userService.AddUser(username, hashedPassword, email);
-            //var response = new RedirectResult("/");
-            //var cookieContent = this.userCookieService.GetUserCookie(username);
-            //var cookie = new HttpCookie(".auth-irunes",cookieContent, 3);
-            //response.AddCookie(cookie);
-
-            //return response;
         }
 
         public IHttpResponse Logout(IHttpRequest request)
