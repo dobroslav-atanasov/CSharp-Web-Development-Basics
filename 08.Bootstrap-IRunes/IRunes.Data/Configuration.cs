349d6f36971f18cb047a53f45313ee7be82670d9
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-08 22:05:49 +0300

    Uploaded project of Bootstrap - Exercises.

---
+++ 08.Bootstrap-IRunes/IRunes.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace IRunes.Data
+{
+    internal class Configuration
+    {
+        internal const string ConnectionString = @"Server=RUMELA-PC\SQLEXPRESS;Database=IRunes;Integrated Security=True";
+    }
+}
