17702a1fb01206afc324bbedfbc69f51f60c80ec
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-13 09:29:11 +0300

    Added project SIS.FrameworkDemo. Created HomeController.cs and Index.html.

---
+++ 09.SimpleMvcFramework/SIS.FrameworkDemo/Laucher.cs
@@ -0,0 +1,16 @@
+﻿namespace SIS.FrameworkDemo
+{
+    using Framework;
+    using Framework.Routers;
+    using WebServer;
+
+    public class Laucher
+    {
+        public static void Main()
+        {
+            var server = new Server(8080, new ControllerRouter()));
+
+            MvcEngine.Run(server);
+        }
+    }
+}
