5d702558fe2ec284333db1d8768893b47164906f
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-19 23:25:49 +0300

    Used services.

--- 09.SimpleMvcFramework/IRunes.App/Launcher.cs
+++ 09.SimpleMvcFramework/IRunes.App/Launcher.cs
@@ -1,17 +1,32 @@
 ﻿namespace IRunes.App
 {
+    using Services;
+    using Services.Contracts;
     using SIS.Framework;
     using SIS.Framework.Routers;
+    using SIS.Framework.Services;
+    using SIS.Framework.Services.Contracts;
     using SIS.WebServer;
 
     public class Launcher
     {
         public static void Main()
         {
-            var handlers = new HttpHandlerContextRouter(new ControllerRouter(), new ResourceRouter());
+            var services = ConfigureServices();
+
+            var handlers = new HttpHandlerContextRouter(new ControllerRouter(services), new ResourceRouter());
             var server = new Server(8080, handlers);
 
-            MvcEngine.Run(server);
+            MvcEngine.Run(server);  
+        }
+
+        private static IDependencyContainer ConfigureServices()
+        {
+            var services = new DependencyContainer();
+
+            services.RegisterDependency<IUserService, UserService>();
+
+            return services;
         }
     }
 }
