e3ec726ae71550087f79139f50d413588bf3beb0
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-19 23:02:07 +0300

    Implemented HomeController.

--- 09.SimpleMvcFramework/IRunes.App/Controllers/HomeController.cs
+++ 09.SimpleMvcFramework/IRunes.App/Controllers/HomeController.cs
@@ -9,6 +9,14 @@
         [HttpGet]
         public IActionResult Index()
         {
+            if (this.IsLoggedIn())
+            {
+                var username = this.Request.Session.GetParameter("username");
+                this.Model.Data["Username"] = username;
+
+                return this.View("Index-Logged-In");
+            }
+
             return this.View();
         }
     }
