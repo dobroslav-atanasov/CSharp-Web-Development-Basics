4cfddeb64283868cd6dab20a49df1eb696749aa1
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-19 22:25:10 +0300

    Added IRunes.App project.

---
+++ 09.SimpleMvcFramework/IRunes.App/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿namespace IRunes.App.Controllers
+{
+    using SIS.Framework.ActionResults.Contracts;
+    using SIS.Framework.Attributes.Methods;
+    using SIS.Framework.Controllers;
+
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ 09.SimpleMvcFramework/IRunes.App/Launcher.cs
@@ -0,0 +1,17 @@
+﻿namespace IRunes.App
+{
+    using SIS.Framework;
+    using SIS.Framework.Routers;
+    using SIS.WebServer;
+
+    public class Launcher
+    {
+        public static void Main()
+        {
+            var handlers = new HttpHandlerContextRouter(new ControllerRouter(), new ResourceRouter());
+            var server = new Server(8080, handlers);
+
+            MvcEngine.Run(server);
+        }
+    }
+}
