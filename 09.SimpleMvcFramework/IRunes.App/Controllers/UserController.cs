e5e69f7285dad577e53a7b903c1053e4ad9c0489
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-19 23:14:38 +0300

    Added UserController.cs and implemented HttpGet Register.

---
+++ 09.SimpleMvcFramework/IRunes.App/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿namespace IRunes.App.Controllers
+{
+    using SIS.Framework.ActionResults.Contracts;
+    using SIS.Framework.Attributes.Methods;
+    using SIS.Framework.Controllers;
+    using ViewModels;
+
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserRegisterViewModel model)
+        {
+            return null;
+        }
+    }
+}
