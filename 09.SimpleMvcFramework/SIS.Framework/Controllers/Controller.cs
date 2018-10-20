9e6621c43031a7b8eafbda6bc7df080ef963fec8
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-20 10:37:35 +0300

    Modified Controller.cs (Response property and SignInUser method)

--- 09.SimpleMvcFramework/SIS.Framework/Controllers/Controller.cs
+++ 09.SimpleMvcFramework/SIS.Framework/Controllers/Controller.cs
@@ -3,7 +3,11 @@
     using System.Runtime.CompilerServices;
     using ActionResults;
     using ActionResults.Contracts;
+    using HTTP.Cookies;
+    using HTTP.Enums;
     using HTTP.Requests.Contracts;
+    using HTTP.Responses;
+    using HTTP.Responses.Contracts;
     using Models;
     using Utilities;
     using Views;
@@ -14,9 +18,12 @@
         {
             this.Model = new ViewModel();
             this.ModelState = new Model();
+            this.Response = new HttpResponse(HttpResponseStatusCode.Ok);
         }
 
         public IHttpRequest Request { get; set; }
+
+        public IHttpResponse Response { get; set; }
 
         protected ViewModel Model { get; }
 
@@ -41,6 +48,12 @@
         protected bool IsLoggedIn()
         {
             return this.Request.Session.ContainsParameter("username");
+        }
+
+        protected void SignInUser(string username, string userCookie)
+        {
+            this.Request.Session.AddParameter("username", username);
+            this.Response.Cookies.Add(new HttpCookie(HttpCookie.Auth, userCookie));
         }
     }
 }
