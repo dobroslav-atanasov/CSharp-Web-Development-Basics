562d6f41cdb826321c1c0f2e9685864ac5e4f48a
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-22 22:30:21 +0300

    Removed Configuration.cs

--- 09.SimpleMvcFramework/IRunes.Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace IRunes.Data
-{
-    internal class Configuration
-    {
-        internal const string ConnectionString = @"Server=RUMELA-PC\SQLEXPRESS;Database=RunesDb;Integrated Security=True";
-    }
-}
