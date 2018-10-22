b952c026d951c128a258de52487aafb56afbf222
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-22 19:30:32 +0300

    Implemented new ActionResult - UnauthorizedResult

---
+++ 09.SimpleMvcFramework/SIS.Framework/ActionResults/UnauthorizedResult.cs
@@ -0,0 +1,19 @@
+﻿namespace SIS.Framework.ActionResults
+{
+    using System.Text;
+    using HTTP.Enums;
+    using HTTP.Headers;
+    using HTTP.Responses;
+
+    public class UnauthorizedResult : HttpResponse
+    {
+        private const string DefaultErrorHandling = "<h1>You have not permission to access this functionality.</h1>";
+
+        public UnauthorizedResult()
+            :base(HttpResponseStatusCode.Unauthorized)
+        {
+            this.Headers.Add(new HttpHeader(HttpHeader.ContentType, "text/html"));
+            this.Content = Encoding.UTF8.GetBytes(DefaultErrorHandling);
+        }
+    }
+}
