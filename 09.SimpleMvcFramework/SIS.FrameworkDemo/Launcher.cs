bf296a75b9cd940df9c8287ed2ef9d2bf313adf9
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-22 19:32:46 +0300

    Modified Launcher.cs in SIS.FrameworkDemo and SIS.SecurityDemo.

--- 09.SimpleMvcFramework/SIS.FrameworkDemo/Launcher.cs
+++ 09.SimpleMvcFramework/SIS.FrameworkDemo/Launcher.cs
@@ -1,17 +1,31 @@
 ﻿namespace SIS.FrameworkDemo
 {
+    using Controllers;
     using Framework;
     using Framework.Routers;
+    using Framework.Services;
+    using Framework.Services.Contracts;
     using WebServer;
 
     public class Launcher
     {
         public static void Main()
         {
-            var handlers = new HttpHandlerContextRouter(new ControllerRouter(), new ResourceRouter());
+            var services = ConfigureServices();
+
+            var handlers = new HttpHandlerContextRouter(new ControllerRouter(services), new ResourceRouter());
             var server = new Server(8080, handlers);
 
             MvcEngine.Run(server);
+        }
+
+        private static IDependencyContainer ConfigureServices()
+        {
+            var services = new DependencyContainer();
+
+            services.RegisterDependency<HomeController, HomeController>();
+
+            return services;
         }
     }
 }
