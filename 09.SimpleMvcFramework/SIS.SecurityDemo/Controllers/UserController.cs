1fb025e95318702b80fcecdb44d08bcf1c0dc8de
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-22 19:28:53 +0300

    Inserted all files in SIS.SecurityDemo.

---
+++ 09.SimpleMvcFramework/SIS.SecurityDemo/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿namespace SIS.SecurityDemo.Controllers
+{
+    using Framework.ActionResults;
+    using Framework.ActionResults.Contracts;
+    using Framework.Attributes.Action;
+    using Framework.Controllers;
+    using Framework.Security;
+
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            this.SignIn(new IdentityUser
+            {
+                Username = "Dobri", Password = "1234"
+            });
+
+            return this.View();
+        }
+
+        [Authorize]
+        public IActionResult Authorized()
+        {
+            this.Model["username"] = this.Identity.Username;
+
+            return new RedirectResult("/Home/Index");
+        }
+    }
+}
