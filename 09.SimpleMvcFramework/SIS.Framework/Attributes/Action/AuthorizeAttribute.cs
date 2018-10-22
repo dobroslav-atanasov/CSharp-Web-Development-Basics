dd641f0c8203a96560ac8d4929dece1021b8bb0b
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-22 19:29:51 +0300

    Created new attribute - Authorize

---
+++ 09.SimpleMvcFramework/SIS.Framework/Attributes/Action/AuthorizeAttribute.cs
@@ -0,0 +1,45 @@
+﻿namespace SIS.Framework.Attributes.Action
+{
+    using System;
+    using System.Linq;
+    using Security.Contracts;
+
+    public class AuthorizeAttribute : Attribute
+    {
+        private readonly string[] roles;
+
+        public AuthorizeAttribute()
+        {
+        }
+
+        public AuthorizeAttribute(string[] roles)
+        {
+            this.roles = roles;
+        }
+
+        public bool IsAuthorized(IIdentity identity)
+        {
+            if (this.roles == null || !this.roles.Any())
+            {
+                return this.IsIdentityPresent(identity);
+            }
+
+            return this.IsIdentityRole(identity);
+        }
+
+        private bool IsIdentityPresent(IIdentity identity)
+        {
+            return identity != null;
+        }
+
+        private bool IsIdentityRole(IIdentity identity)
+        {
+            if (!this.IsIdentityPresent(identity))
+            {
+                return false;
+            }
+
+            return roles.Any(role => identity.Roles.Contains(role));
+        }
+    }
+}
