181778bff48237a0666a8637ace622e55b52322d
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-28 16:05:32 +0200

    Created UsersController.cs and implemented logic of Login and Register.

---
+++ Exam-MishMash/MishMash.App/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿namespace MishMash.App.Controllers
+{
+    using SIS.Framework.ActionResults;
+    using SIS.Framework.Attributes.Method;
+    using SIS.Framework.Controllers;
+
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return this.View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return this.View();
+        }
+    }
+}
