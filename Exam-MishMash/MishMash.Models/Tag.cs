996b9a07b9e4cb84921e710d58a7300e05453cdd
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-10-28 15:36:27 +0200

    Added project MishMash.Models and implemented all models.

---
+++ Exam-MishMash/MishMash.Models/Tag.cs
@@ -0,0 +1,12 @@
+﻿namespace MishMash.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int ChannelId { get; set; }
+        public Channel Channel { get; set; }
+    }
+}
