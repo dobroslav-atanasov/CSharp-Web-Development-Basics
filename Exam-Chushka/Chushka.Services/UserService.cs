5ccb953dd53dd7239f39b9d8ce81559077abfbf5
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-11-01 22:46:09 +0200

    Created IUserService.cs and UserService.cs. Implemented all methods.

---
+++ Exam-Chushka/Chushka.Services/UserService.cs
@@ -0,0 +1,58 @@
+﻿namespace Chushka.Services
+{
+    using System.Linq;
+    using Contracts;
+    using Data;
+    using Models;
+    using Models.Enums;
+
+    public class UserService : IUserService
+    {
+        public bool IsUserExists(string username)
+        {
+            using (var db = new ChushkaDbContext())
+            {
+                return db.Users.Any(u => u.Username == username);
+            }
+        }
+
+        public void AddUser(string username, string password, string fullName, string email)
+        {
+            using (var db = new ChushkaDbContext())
+            {
+                var user = new User
+                {
+                    Username = username,
+                    Password = password,
+                    FullName = fullName,
+                    Email = email,
+                    Role = Role.User
+                };
+
+                if (!db.Users.Any())
+                {
+                    user.Role = Role.Admin;
+                }
+
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+        }
+
+        public User GetUserByUsername(string username)
+        {
+            using (var db = new ChushkaDbContext())
+            {
+                return db.Users.FirstOrDefault(u => u.Username == username);
+            }
+        }
+
+        public User GetUserByUsernameAndPassword(string username, string password)
+        {
+            using (var db = new ChushkaDbContext())
+            {
+                return db.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+            }
+        }
+    }
+}
